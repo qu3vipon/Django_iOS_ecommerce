5bebc37dc1a5bc42dfc3ca7105130d28ab7901a8
eunbiviakwon <eunbiviakwon@gmail.com>
2020-03-18 20:12:39 +0900

    conf: RDS로 postgresql 연결

--- app/config/settings.py
+++ app/config/settings.py
@@ -91,12 +91,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = SECRETS['DATABASES']
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
