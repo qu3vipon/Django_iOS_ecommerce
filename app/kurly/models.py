3aa830c33c713731bd45dbbbffe812dc89b859d7
qu3vipon <sdh5813@gmail.com>
2020-03-30 11:31:02 +0900

    conf: model 수정

--- app/kurly/models.py
+++ app/kurly/models.py
@@ -10,26 +10,27 @@ class Order(models.Model):
     ]
 
     PAYMENT_CHOICES = [
-        ('card', '카드 결제'),
-        ('mobile', '휴대폰 결제'),
-        ('others', '기타 결제'),
+        ('card', '카드결제'),
+        ('mobile', '휴대폰결제'),
+        ('others', '기타결제'),
     ]
     STATUS_CHOICES = [
-        ('yet', '배송 준비 중'),
-        ('on', '배송 중'),
-        ('done', '배송 완료'),
+        ('yet', '배송준비중'),
+        ('on', '배송중'),
+        ('done', '배송완료'),
     ]
     buyer = models.ForeignKey(User, on_delete=models.CASCADE)
     receiver = models.CharField(max_length=30)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='orders')
     delivery = models.CharField(choices=DELIVERY_CHOICES, max_length=20)
     mobile = models.PositiveIntegerField()
     requirements = models.TextField(max_length=100, blank=True)
     total_price = models.PositiveIntegerField()
 
     ordered = models.BooleanField(default=False)
-    ordered_at = models.DateTimeField(auto_now=True)
+    ordered_at = models.DateTimeField()
     payment = models.CharField(choices=PAYMENT_CHOICES, max_length=20)
+    payed_at = models.DateTimeField()
     agreed = models.BooleanField(default=False)
 
     status = models.CharField(choices=STATUS_CHOICES, max_length=10, default='yet')
@@ -47,7 +48,8 @@ class Subcategory(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=50)
     summary = models.CharField(max_length=50)
-    subcategory = models.OneToOneField(Subcategory, on_delete=models.SET_NULL, null=True)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE, related_name='products')
+
     price = models.PositiveIntegerField()
     unit = models.CharField(max_length=10)
     amount = models.CharField(max_length=30)
@@ -57,7 +59,7 @@ class Product(models.Model):
 
     sales = models.PositiveIntegerField(default=0)
     stock = models.PositiveSmallIntegerField(default=99)
-    created = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Image(models.Model):
@@ -66,14 +68,14 @@ class Image(models.Model):
     image = models.ImageField(upload_to='')
 
 
-class OrderProduct(models.Model):
-    order = models.ForeignKey(Order, related_name='orderproducts', on_delete=models.CASCADE)
-    product = models.OneToOneField(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveSmallIntegerField(default=1)
-    option = models.PositiveIntegerField(blank=True, null=True)
-
-
 class Option(models.Model):
     product = models.ForeignKey(Product, related_name='options', on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     price = models.PositiveIntegerField()
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, related_name='orderproducts', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='orderproducts')
+    option = models.ForeignKey(Option, blank=True, null=True, related_name='orderproducts', on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=1)
