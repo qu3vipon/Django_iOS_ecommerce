45716ef91ef4ce67227df84233c320f4ec4ced01
qu3vipon <sdh5813@gmail.com>
2020-04-08 14:50:35 +0900

    feat: 장바구니 정보 update API

--- app/members/urls.py
+++ app/members/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.UserRetrieveView.as_view()),
+    path('<int:pk>/', views.UserDetailView.as_view()),
     path('', views.UserCreateView.as_view()),
     path('duplicates/', views.CheckDuplicatesView.as_view()),
     path('m-token-create/', views.MobileTokenCreateView.as_view()),
